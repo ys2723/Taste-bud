c112fd84d112d496017b267267b79e36cb2e8881
NeilB1ju <95778971+NeilB1ju@users.noreply.github.com>
2023-08-19 02:20:16 +0530

    updated home page animation

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 
 const API_KEY = 'api_key=774c45332d4ea2aafdb9848019af87e5';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
@@ -189,7 +189,6 @@ fetch(API_URL)
         movieImage.src =IMG_URL+ movies[i].poster_path;
         movieImage.alt = movies[i].title + ' Poster';
         card.appendChild(movieImage);
-
         popularMoviesSection.appendChild(card);
       }
 
