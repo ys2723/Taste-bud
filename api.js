003208899ac4be0581ceb9fad90e9ee2b8993de5
Pallav Raj <103437428+pn1027@users.noreply.github.com>
2023-08-18 16:38:10 +0530

    Push 1 P

--- api.js
+++ api.js
@@ -1,10 +1,8 @@
-
 const API_KEY = 'api_key=774c45332d4ea2aafdb9848019af87e5';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
-
 
 const genres = [
     {
@@ -85,22 +83,9 @@ const genres = [
     }
   ]
 
-  const main = document.getElementById('main');
-  const form = document.getElementById('form');
-  const search = document.getElementById('search');
-  const tagsEl = document.getElementById('tags');
-  
-  const displaySection = document.querySelector('.display-section');
-
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const current = document.getElementById('current')
-
-var currentPage = 1;
-var nextPage = 2;
-var prevPage = 3;
-var lastUrl = '';
-var totalPages = 100;
+const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
 
 var selectedGenre = []
 setGenre();
@@ -148,6 +133,26 @@ function highlightSelection() {
 
 }
 
+function clearBtn(){
+    let clearBtn = document.getElementById('clear');
+    if(clearBtn){
+        clearBtn.classList.add('highlight')
+    }else{
+            
+        let clear = document.createElement('div');
+        clear.classList.add('tag','highlight');
+        clear.id = 'clear';
+        clear.innerText = 'Clear x';
+        clear.addEventListener('click', () => {
+            selectedGenre = [];
+            setGenre();            
+            getMovies(API_URL);
+        })
+        tagsEl.append(clear);
+    }
+    
+}
+
 getMovies(API_URL);
 
 function getMovies(url) {
@@ -186,75 +191,82 @@ function getMovies(url) {
 
 
 function showMovies(data) {
-  const popularMovies = document.getElementById('popular-movies');
-  popularMovies.innerHTML = '';
+    main.innerHTML = '';
 
-  data.forEach(movie => {
-      const { title, poster_path, vote_average } = movie;
-      const movieEl = document.createElement('div');
-      movieEl.classList.add('card');
-      movieEl.innerHTML = `
-          <img src="${poster_path ? IMG_URL + poster_path : 'http://via.placeholder.com/300x450'}" alt="${title}">
-          <div class="card-info">
-              <h3>${title}</h3>
-              <span class="${getColor(vote_average)}">${vote_average}</span>
-          </div>
-      `;
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview, id} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
 
-      popularMovies.appendChild(movieEl);
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
 
-      movieEl.addEventListener('click', () => {
-          openNav(movie);
-      });
-  });
-}
+            <div class="overview">
 
-const overlayContent = document.getElementById('overlay-content');
-/* Open when someone clicks on the span element */
-function openNav(movie) {
-  let id = movie.id;
-  fetch(BASE_URL + '/movie/'+id+'/videos?'+API_KEY).then(res => res.json()).then(videoData => {
-    console.log(videoData);
-    if(videoData){
-      document.getElementById("myNav").style.width = "100%";
-      if(videoData.results.length > 0){
-        var embed = [];
-        var dots = [];
-        videoData.results.forEach((video, idx) => {
-          let {name, key, site} = video
-
-          if(site == 'YouTube'){
-              
-            embed.push(`
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          
-          `)
-
-            dots.push(`
-              <span class="dot">${idx + 1}</span>
-            `)
-          }
-        })
-        
-        var content = `
-        <h1 class="no-results">${movie.original_title}</h1>
-        <br/>
-        
-        ${embed.join('')}
-        <br/>
-
-        <div class="dots">${dots.join('')}</div>
+                <h3>Overview</h3>
+                ${overview}
+                <br/> 
+                <button class="know-more" id="${id}">Know More</button
+            </div>
         
         `
-        overlayContent.innerHTML = content;
-        activeSlide=0;
-        showVideos();
-      }else{
-        overlayContent.innerHTML = `<h1 class="no-results">No Results Found</h1>`
-      }
-    }
-  })
+
+        main.appendChild(movieEl);
+
+        document.getElementById(id).addEventListener('click', () => {
+          console.log(id)
+          openNav(movie)
+        })
+    })
 }
 
+/* Close when someone clicks on the "x" symbol inside the overlay */
 
-  
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    selectedGenre=[];
+    setGenre();
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+
+})
+
+prev.addEventListener('click', () => {
+  if(prevPage > 0){
+    pageCall(prevPage);
+  }
+})
+
+next.addEventListener('click', () => {
+  if(nextPage <= totalPages){
+    pageCall(nextPage);
+  }
+})
+
+function pageCall(page){
+  let urlSplit = lastUrl.split('?');
+  let queryParams = urlSplit[1].split('&');
+  let key = queryParams[queryParams.length -1].split('=');
+  if(key[0] != 'page'){
+    let url = lastUrl + '&page='+page
+    getMovies(url);
+  }else{
+    key[1] = page.toString();
+    let a = key.join('=');
+    queryParams[queryParams.length -1] = a;
+    let b = queryParams.join('&');
+    let url = urlSplit[0] +'?'+ b
+    getMovies(url);
+  }
+}
+
